42e37807f9b91f39d6ec4079b837495dbae95f44
elif <ozunelif0@gmail.com>
2025-07-09 13:37:28 +0300

    Initial commit - only appearances will be changed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkgreen: '#166534',
+        forest: '#14532D',
+        beige: '#E5E5C9',
+        offwhite: '#F7F7F7',
+      },
+    },
+  },
+  plugins: [],
+}
